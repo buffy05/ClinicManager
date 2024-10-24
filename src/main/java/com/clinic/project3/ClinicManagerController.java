063c496f1c06e58be99eef3fab4d1c4e08e79cc0
buffy05 <syonab9@gmail.com>
2024-10-23 23:15:03 -0400

    yuh

--- src/main/java/com/clinic/project3/ClinicManagerController.java
+++ src/main/java/com/clinic/project3/ClinicManagerController.java
@@ -3,12 +3,12 @@ package com.clinic.project3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ClinicManagerController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome bitch");
     }
 }
